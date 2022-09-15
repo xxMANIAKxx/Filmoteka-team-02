2dcefc42639954fee0be94595453f6b22ef2e991
GrzegorzKobuszewski <kobuszewskigrzegorz@gmail.com>
2022-09-15 22:52:07 +0200

    Gatunki-Filmów-Działające

--- src/js/fetchData.js
+++ src/js/fetchData.js
@@ -1,19 +1,26 @@
 const inputFormTitle = document.querySelector('.search-form__input');
 const inputFormButton = document.querySelector('.search-form__btn');
 const inputFormGenreChange = document.querySelector('.header__genre-option');
+
 const galleryOfMovies = document.querySelector('.gallery_movies');
+const genresList = document.querySelector('.genres');
 const paginationButtons = document.querySelector('.pagination_buttons');
 
+// SPIS TREŚCI:
 // Scenariusz 1: FIRST LOAD
 // Scenariusz 2: SEARCH MOVIE
 // Scenariusz 3: GENRE SELECT
 // Scenariusz 4: TRENDING DAY / TRENDING WEEK
 
+// Ustalanie gatunku/gatunków filmu
+// Paginacja
+
 const API_KEY = '?api_key=fd87aef18dfd3a2446d882cb85b7272d';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const MAIN_PAGE_URL = '/trending/all/day';
 const SEARCH_MOVIE_URL = '/search/movie';
-const GENRE_SELECT_URL = '/genre/movie/list';
+const GENRE_MOVIE_LIST_URL = '/genre/movie/list';
+const GENRE_TV_LIST_URL = '/genre/tv/list';
 const TRENDING_DAY_URL = '/trending/movie/day';
 const TRENDING_WEEK_URL = '/trending/movie/week';
 
@@ -22,7 +29,9 @@ let page = 1;
 // Scenariusz 1: FIRST LOAD krok 1
 // Pobranie danych do galerii, która wyświetla się po WEJŚCIU na stronę
 const fetchFirstLoadMovies = async () => {
-  const response = await fetch(`${BASE_URL}${MAIN_PAGE_URL}${API_KEY}&page=${page}`);
+    const response = await fetch(
+        `${BASE_URL}${MAIN_PAGE_URL}${API_KEY}&page=${page}`
+    );
   const firstLoadMovies = await response.json();
   return firstLoadMovies;
 };
@@ -44,33 +53,21 @@ const fetchMovieById = async (movieId, type = 'movie') => {
   return responseObject;
 };
 
+// Scenariusz 1: FIRST LOAD krok 2
 // Tworzenie galerii filmów po WEJŚCIU na stronę (lub przeładowaniu)
-let renderMoviesFirstLoad = data => {
+let renderMoviesFirstLoad = async data => {
+  const genreName = await getMovieGenresNames();
   galleryOfMovies.innerHTML = '';
   const markup = data
-    .map(
-      ({
-        poster_path,
-        original_title,
-        title,
-        genre_ids,
-        media_type,
-        release_date,
-        vote_average,
-        name,
-        original_name,
-        id,
-      }) => {
+        .map(({poster_path, title, name, genre_ids, release_date, first_air_date, vote_average, id, media_type, original_title, original_name}) => {
         return `
-                <li class="movie-card" data-id="${id}" data-type="${media_type}">
-                    <img class="movie-card__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${
-          title === undefined ? name : title
-        }" loading="lazy" />
+                <li class="movie-card">
+                    <img class="movie-card__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="poster of "${title === undefined ? name : title}"" loading="lazy" />
                     <h2 class="movie-card__title">${title === undefined ? name : title}</h2>
                     <div class="movie-card__info">
                         <p class="movie-card__genre-and-year">
-                            <span class="movie-card__genre">${genre_ids}</span>
-                            <span class="movie-card__year">${release_date}</span>
+                            <span class="movie-card__genre">${genre_ids.map(id => genreName[id]).join(', ')}</span>
+                            <span class="movie-card__year">${release_date || first_air_date}</span>
                         </p>
                         <p class="movie-card__vote-average">${vote_average}</p>
                     </div>
@@ -81,46 +78,148 @@ let renderMoviesFirstLoad = data => {
     .join('');
   return galleryOfMovies.insertAdjacentHTML('beforeend', markup);
 };
+
 
 // Scenariusz 2: SEARCH MOVIE krok 2
 // Tworzenie galerii po WPISANIU FILMU w input
-let renderMoviesInputTitle = data => {
+let renderMoviesInputTitle = async data => {
+  const genreName = await getMovieGenresNames();
   galleryOfMovies.innerHTML = '';
   const markup = data
-    .map(
-      ({
-        poster_path,
-        original_title,
-        title,
-        genre_ids,
-        media_type,
-        release_date,
-        vote_average,
-        original_name,
-        name,
-        id,
-      }) => {
+        .map(({poster_path, title, name, genre_ids, release_date, first_air_date, vote_average, id, media_type, original_title, original_name}) => {
         return `
                 <li class="movie-card" data-id="${id}" data-type="${media_type}">
-                    <img class="movie-card__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${
-          title === undefined ? name : title
-        }" loading="lazy" />
+                    <img class="movie-card__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${title === undefined ? name : title}" loading="lazy" />
                     <h2 class="movie-card__title">${title === undefined ? name : title}</h2>
                     <div class="movie-card__info">
                         <p class="movie-card__genre-and-year">
-                            <span class="movie-card__genre">${genre_ids}</span>
-                            <span class="movie-card__year">${release_date}</span>
+                            <span class="movie-card__genre">${genre_ids.map(id => genreName[id]).join(', ')}</span>
+                            <span class="movie-card__year">${release_date || first_air_date}</span>
                         </p>
                         <p class="movie-card__vote-average">${vote_average}</p>
                     </div>
                 </li>
-            `;
-      },
+            `},
     )
     .join('');
   return galleryOfMovies.insertAdjacentHTML('beforeend', markup);
 };
 
+
+//-----------------------------------------------------------------//
+// ZNALEZIENIE LISTY WSZYSTKICH GATUNKÓW FILMÓW
+// z obu podzbiorów bazy danych: Movie oraz TV
+
+const getAllGenres = async () => {
+    const responseGenresMovie = await fetch(
+        `${BASE_URL}${GENRE_MOVIE_LIST_URL}${API_KEY}&language=en-US`
+    );
+    const responseGenresTV = await fetch(
+        `${BASE_URL}${GENRE_TV_LIST_URL}${API_KEY}&language=en-US`
+    );
+  
+    const genresMovieList = await responseGenresMovie.json();
+    const genresTVList = await responseGenresTV.json();
+  
+    const allGenresList = [
+        ...new Map(
+            [...genresMovieList.genres, ...genresTVList.genres].map(genre => [
+                genre['id'], genre,
+            ])
+        ).values(),
+    ];
+    return allGenresList;
+};
+
+let genreResponse;
+
+const getMovieGenresNames = async () => {
+    if (!genreResponse) {
+        genreResponse = await getAllGenres();
+    }
+    return genreResponse.reduce((allGenres, genre) => {
+        return {...allGenres, [genre.id]: genre.name,
+        };
+    }, {});
+};
+
+
+
+
+
+
+
+let printAllGenresList = () => {
+    genresList.innerHTML = '';
+    const markup = allGenresList
+        .map(genre => {
+            return `
+                <div>
+                    <ul>
+                        <li class="genres">${genre}</li>
+                    </ul>
+                </div>
+            `})
+        .join("");
+    return genresList.insertAdjacentHTML('beforeend', markup);
+};
+
+
+
+
+
+
+/*
+
+//-----------------------------------------------------------------//
+
+let genresList = [];
+
+
+// Funkcja, która pyta o gatunki i zwraca je na liście rozwijanej
+
+function fetchIDFilms() {
+  return newFilmsBandle
+    .onFetchId()
+    .then(genres => {
+      genresList = genres;
+      renderGenresList(genres);
+      return genresList;
+    })
+    .then(genresList => localeStorageServices.save('FilmIDs', genresList))
+    .catch(console.log);
+}
+
+
+
+
+// Funkcja zmieniająca identyfikatory gatunków na ich NAZWY
+
+function parsGenres(genresId, genresList) {
+  const nameGenres = [];
+  for (let i = 0; i <= genresId.length; i += 1) {
+    genresList.map(({ id, name }) => {
+      if (id === genresId[i]) {
+        nameGenres.push(name);
+      }
+    });
+  }
+  return nameGenres;
+}
+
+
+*/
+
+
+
+
+
+
+
+
+
+//-----------------------------------------------------------------//
+// PAGINACJA
 const pagination = async (totalPages, title) => {
   paginationButtons = '';
   if (totalPages >= 1) {
@@ -131,6 +230,8 @@ const pagination = async (totalPages, title) => {
     }
   }
 };
+
+
 
 export {
   inputFormButton,
